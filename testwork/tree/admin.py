b5649c7c3b824938d626e43a98185e8038ff5199
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2024-08-06 18:19:46 +0300

    0.1

--- testwork/tree/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Menu
-
-
-class Menu(admin.ModelAdmin):
-    list_display = ('title',)  # Отображение поля title в списке
-
-
-admin.site.register(Menu)
